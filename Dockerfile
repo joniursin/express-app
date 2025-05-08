3c6d273992af3590ff5cd6c661fec187332e5445
Joni Ursin <joni.ursin@gmail.com>
2025-05-09 00:30:02 +0300

    updated hello watchtower

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,4 @@ COPY . .
 
 RUN npm install
 
-LABEL com.centurylinklabs.watchtower.enable="true"
-
 CMD node index.js
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Watchtower 4!</h1>')
+  res.send('<h1>Hello Watchtower 5!</h1>')
 })
 
 const PORT = 8080
